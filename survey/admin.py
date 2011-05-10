bc2ed1ac284530507a162afceacf875ad8ad3d1a
Matthias Kestenholz <mk@spinlock.ch>
2011-05-10 12:55:25 +0200

    Improve admin interface

--- survey/admin.py
+++ survey/admin.py
@@ -16,18 +16,18 @@ class QuestionInline(admin.TabularInline):
 admin.site.register(models.Survey,
     inlines=[QuestionGroupInline],
     list_display=('is_active', 'title', 'code'),
-    list_display_links=('title',),
+    list_display_links=('title', 'code'),
     list_filter=('is_active',),
     )
 admin.site.register(models.QuestionGroup,
     inlines=[QuestionInline],
     list_display=('survey', 'ordering', 'title'),
-    list_display_links=('title',),
+    list_display_links=('title', 'ordering'),
     list_filter=('survey',),
     )
 admin.site.register(models.Question,
     list_display=('group', 'ordering', 'text', 'has_importance', 'type'),
-    list_display_links=('text',),
+    list_display_links=('text', 'ordering'),
     list_filter=('group__survey',),
     )
 
@@ -37,7 +37,7 @@ admin.site.register(models.SurveyAnswer,
             'fields': ('survey', 'code', 'status'),
         }),
         (_('Visitor'), {
-            'fields': (('visitor_company', 'visitor_name', 'visitor_contact'),),
+            'fields': (('visitor_company', 'visitor_name', 'visitor_contact', 'visitor_counter'),),
         }),
         (_('Conductor'), {
             'fields': (('conductor_company', 'conductor_name', 'conductor_contact'),),
@@ -48,6 +48,6 @@ admin.site.register(models.SurveyAnswer,
         }),
         ),
     list_display=('survey', 'status', 'code', 'visitor_name', 'visitor_contact'),
-    list_display_links=('code',),
-    list_filter=('survey', 'status', 'conductor_name'),
+    list_display_links=('code', 'visitor_name', 'visitor_contact'),
+    list_filter=('survey', 'status', 'conductor_name', 'conductor_contact'),
     )
